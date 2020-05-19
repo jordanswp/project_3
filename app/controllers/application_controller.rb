0129e5ae7324657b25a95b8f830cbe91861f2367
kokchuantan <kokchuan1996@gmail.com>
2020-05-19 16:15:36 +0800

    added function to add/show listings

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,2 @@
 class ApplicationController < ActionController::Base
-  before_action :configure_permitted_parameters, if: :devise_controller?
-
-  protected
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:username], [:contact])
-  end
-
-  def
-    devise_parameter_sanitizer.permit(:sign_in) do |user_params|
-    user_params.permit(:username, :email, :contact)
-  end
 end
--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -4,8 +4,23 @@ class ListingsController < ApplicationController
         @categories = Category.all
         @listings = Listing.all
     end
-
+    def new
+        @categories = Category.all
+    end
+    def create
+        @listing = Listing.new(listing_params)
+        @listing.user = current_user
+        if @listing.save
+            redirect_to @listing
+          else
+            render 'new'
+          end
+    end
+    def show
+        @listing = Listing.find(params[:id])
+    end
+    
   def listing_params
-    params.require(:listing).permit(:title, :image_url, :price, :description)
+    params.require(:listing).permit(:title, :image_url, :price, :description, :category_id)
   end
 end
