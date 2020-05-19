64afd2bef1e96868e907ba5833fbe2a67dc5e26f
dannst <arcanic@gmail.com>
2020-05-20 01:58:25 +0800

    add basic comment functionalities, javascript for DOM manipulation, comment section render under views/listings/show

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,21 @@
 class CommentsController < ApplicationController
 
+  def create
+    @listing = Listing.find(params[:listing_id])
+    @comment = Comment.new(comment_params)
+    @comment.listing = @listing
+    @comment.user = current_user
 
+    if @comment.save 
+      redirect_to @listing
+    else
+      render plain: @comment.errors.messages
+    end
+  end
+
+  private
+    def comment_params
+      params.require(:comment).permit(:title, :body)
+    end
 
 end
