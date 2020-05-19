289ce73ce07f0ada104eb3158fe86a78c822aa2e
kokchuantan <kokchuan1996@gmail.com>
2020-05-19 15:12:12 +0800

    added show all listings page

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,2 +1,11 @@
 class ListingsController < ApplicationController
+    before_action :authenticate_user!
+    def index
+        @categories = Category.all
+        @listings = Listing.all
+    end
+
+  def listing_params
+    params.require(:listing).permit(:title, :image_url, :price, :description)
+  end
 end
