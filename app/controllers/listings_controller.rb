f333645ec8a1b6a9958fd1ec94d4ebf26e801def
dannst <arcanic@gmail.com>
2020-05-20 10:50:38 +0800

    add listings controller, index and new

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,3 +1,32 @@
 class ListingsController < ApplicationController
 
+  before_action :authenticate_user!
+
+  def show
+    @listing = Listing.find(params[:id])
+  end
+
+  def new
+    @listing = Listing.new
+  end
+
+  def create
+    @listing = Listing.new(listing_params)
+    @listing.user = current_user
+
+      if @listing.save
+        redirect_to @listing
+      else
+        render plain: @listing.errors.messages
+    end
+  end
+
+private
+    def listing_params
+      params.require(:listing).permit(:title, :image_url, :price, :description, :user_id, :category_id)
+    end
+
+
 end
+
+
