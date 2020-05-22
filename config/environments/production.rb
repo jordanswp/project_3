b761a99dc19d9b086b0194dc421ee54aff4eb74a
dannst <arcanic@gmail.com>
2020-05-22 13:01:25 +0800

    set config.serve_static_assets = true, config.assets.compile = true in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,8 +26,11 @@ Rails.application.configure do
   config.assets.js_compressor = Uglifier.new(harmony: true)
   # config.assets.css_compressor = :sass
 
+  # added 
+  config.serve_static_assets = true
+
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
