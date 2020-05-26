cd7ade6258fa2266edf39aa031542a2889952032
dannst <arcanic@gmail.com>
2020-05-26 16:59:53 +0800

    Modify heroku config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,7 +49,7 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://example.com/cable'
+  config.action_cable.url = "wss://lystene.herokuapp.com/cable"
   config.action_cable.allowed_request_origins = [ 'https://lystene.herokuapp.com', 'http://lystene.herokuapp.com']
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
